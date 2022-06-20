6ec33e61e7c1b13d711e298a4923e269d2befa47
beslimir <marin.beslo@gmail.com>
2022-06-20 14:16:15 +0200

    add image to component item

--- app/src/main/java/com/example/mychallenge/presentation/main_screen/ChallengeItem.kt
+++ app/src/main/java/com/example/mychallenge/presentation/main_screen/ChallengeItem.kt
@@ -1,11 +1,21 @@
 package com.example.mychallenge.presentation.main_screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -13,9 +23,15 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.ImagePainter
+import coil.compose.rememberImagePainter
 import com.example.mychallenge.domain.model.Challenge
+import kotlinx.coroutines.launch
 
 //@Preview
+@ExperimentalCoilApi
 @Composable
 fun ChallengeItem(
     challenge: Challenge
@@ -35,19 +51,40 @@ fun ChallengeItem(
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.Start
         ) {
-            Text(
-                text = challenge.name,
-                color = Color.White,
-                style = TextStyle(
-                    fontWeight = FontWeight.Bold,
-                    fontFamily = FontFamily.SansSerif,
-                    fontSize = 20.sp
-                ),
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                modifier = Modifier
-                    .padding(top = 10.dp, start = 10.dp)
-            )
+            Box {
+                val painter = rememberImagePainter(
+                    data = "https://i.ytimg.com/vi/aZihG8ysDss/maxresdefault.jpg"
+                )
+                val painterState = painter.state
+
+                Image(
+                    painter = painter,
+                    contentDescription = "My Challenge",
+                    modifier = Modifier
+                        .fillMaxSize()
+                )
+                if (painterState is ImagePainter.State.Loading) {
+                    CircularProgressIndicator(
+                        color = MaterialTheme.colors.primary,
+                        modifier = Modifier
+                            .scale(0.5f)
+                            .fillMaxSize()
+                    )
+                }
+                Text(
+                    text = challenge.name,
+                    color = Color.White,
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = FontFamily.SansSerif,
+                        fontSize = 20.sp
+                    ),
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier
+                        .padding(top = 10.dp, start = 10.dp)
+                )
+            }
         }
         Column(
             modifier = Modifier
